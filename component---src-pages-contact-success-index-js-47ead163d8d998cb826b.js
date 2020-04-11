e9772402c1d6052181995a2c15db546fbf681e41
Allie c( ⁰ 〰 ⁰ )੭ <88z@pm.me>
2020-04-11 00:47:40 -0700

    Updates

---
+++ component---src-pages-contact-success-index-js-47ead163d8d998cb826b.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{aWcV:function(e,t,a){"use strict";a.r(t);var s=a("q1tI"),c=a.n(s),n=a("Zttt");t.default=function(){return c.a.createElement(n.a,null,c.a.createElement("section",{className:"hero is-primary is-bold is-large"},c.a.createElement("div",{className:"hero-body has-text-centered"},c.a.createElement("h1",{className:"title"},"Success"))))}}}]);
+//# sourceMappingURL=component---src-pages-contact-success-index-js-47ead163d8d998cb826b.js.map
